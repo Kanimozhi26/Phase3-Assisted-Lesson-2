c682bcbd70980909bc26eaffd565dad667c24f00
Kanimozhi <kanima2651@gmail.com>
2022-08-25 20:33:08 +0530

    first commit

---
+++ Spring_Boot_Download/src/main/java/com/SpringBootDownloadApplication.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDownloadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDownloadApplication.class, args);
+	}
+
+}
